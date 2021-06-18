8dbda98ba4b68af91a867c48434d1d0669977cf5
jakeserok <jakeserok@gmail.com>
2021-06-18 17:02:24 -0400

    Updated application to handle jquery

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,14 +11,31 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
     @goal = Goal.find(params[:goal_id])
     @task = @goal.tasks.create(task_params)
     redirect_to goal_path(@goal), notice: "Task added successfully!"
   end
 
+  def update
+    @task = Task.find(params[:id])
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: "Task was successfully updated." }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def task_params
-      params.require(:task).permit(:item, :description, :goal)
+      params.require(:task).permit(:item, :description, :goal, :is_complete)
     end
 end
