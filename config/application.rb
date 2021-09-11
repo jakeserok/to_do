281781bbdeca7d4d8f18e806b49cdb85e591e933
jakeserok <jakeserok@gmail.com>
2021-09-10 23:48:27 -0400

    Removed legacy option

--- config/application.rb
+++ config/application.rb
@@ -34,9 +34,5 @@ module ToDo
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'ALLOWALL'
-    }
   end
 end
