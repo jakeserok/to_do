f6db47dc4507b66938108d1a516960e54bcb8c11
jakeserok <jakeserok@gmail.com>
2021-09-10 21:15:12 -0400

    Added ALLOWALL for Xframe headers

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,9 @@ module ToDo
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
